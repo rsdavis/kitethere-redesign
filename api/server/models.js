a8e8f6417e64811a749167c5f93a6abeea6245be
Ryan Davis <rsdavis@Ryans-MBP.fios-router.home>
2018-08-22 22:30:35 -0400

    Implemented spot versioning system

--- api/server/models.js
+++ api/server/models.js
@@ -1,5 +1,10 @@
 
 const Sequelize = require('sequelize')
+const uuid = require('uuid/v4')
+const shortid = require('shortid')
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@')
+
+const tableOptions = { underscored: true, timestamps: false, freezeTableName: true }
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -11,6 +16,36 @@ const sequelize = new Sequelize(
     }
 )
 
+const Spot = sequelize.define('spots', {
+    id: { type: Sequelize.STRING, defaultValue: shortid.generate, primaryKey: true }
+}, tableOptions)
+
+const Version = sequelize.define('versions', {
+    id: { type: Sequelize.UUID, defaultValue: uuid, primaryKey: true, allowNull: false },
+    created: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+    name: Sequelize.STRING,
+    lat: Sequelize.DOUBLE,
+    lng: Sequelize.DOUBLE,
+    description: Sequelize.TEXT
+}, tableOptions)
+
+const Section = sequelize.define('sections', {
+    id: { type: Sequelize.UUID, defaultValue: uuid, primaryKey: true, allowNull: false },
+    heading: Sequelize.STRING,
+    body: Sequelize.TEXT
+}, tableOptions)
+
+const Image = sequelize.define('images', {
+    id: { type: Sequelize.UUID, defaultValue: uuid, primaryKey: true, allowNull: false }
+}, tableOptions)
+
+Spot.hasMany(Version, { foreignKey: 'spot_id' })
+Spot.belongsTo(Version, { as: 'CurrentVersion', constraints: false })
+
+Version.hasMany(Section)
+Version.hasMany(Image)
+
+sequelize.drop()
 sequelize.sync({ force: true })
 
-module.exports = sequelize
+module.exports = { sequelize, Spot, Version, Section, Image }
